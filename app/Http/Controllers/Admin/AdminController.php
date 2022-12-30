cde1798c8a0c7855b86135443f580a8fc7269db0
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2022-12-30 23:47:09 +0700

    add admin template

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function index() {
+        $numberUser = DB::table('users')->where('roleId', 2)->count();
+        $numberService = DB::table('services')->count();
+        $numberFood = DB::table('food')->count();
+        $numberOrder = DB::table('orders')->count();
+        $services = Service::all();
+        return view('admin.index')->with('services',$services)
+                                ->with('numberUser', $numberUser)
+                                ->with('numberService',$numberService)
+                                ->with('numberFood',$numberFood)
+                                ->with('numberOrder',$numberOrder);
+    }
+}
