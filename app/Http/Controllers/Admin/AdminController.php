0d0341d966f7a6a3ea73c4a2e07d5a43d08d9450
thuyhang <thuyhangfr01@gmail.com>
2023-01-06 12:57:14 +0700

    statistic by date

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Service;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\Statistic;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
 
 class AdminController extends Controller
  {
     public function index() {
-
         $numberUser = DB::table('users')->where('roleId', 2)->count();
         $numberService = DB::table('services')->count();
         $numberFood = DB::table('food')->count();
@@ -34,7 +38,43 @@ class AdminController extends Controller
                                 ->with('orderLists', $orderLists);
     }
 
-    public function showNoti(){
+    public function filter_by_date(Request $request){
+        $from_date = Carbon::createFromFormat('m/d/Y', $request->from_date)->format('Y-m-d');
+        $to_date = Carbon::createFromFormat('m/d/Y', $request->to_date)->format('Y-m-d');
+
+        // $gets = DeclarationStatistic::all();
+        $gets = Statistic::whereBetween('date',[$from_date, $to_date])->orderBy('date','ASC')->get();
+        $chart_data = new Collection();
+        foreach($gets as $get){
+            $chart_data->push([
+                'date' => $get->date,
+                'sumRevenues' => $get->sumRevenues
+            ]);
+        }
+        return json_encode($chart_data);
+    }
+
+    public function filter_by_date1(Request $request){
+        $from_date = Carbon::createFromFormat('m/d/Y', $request->from_date)->format('Y-m-d');
+        $to_date = Carbon::createFromFormat('m/d/Y', $request->to_date)->format('Y-m-d');
+        $serviceId = $request->serviceId;
+        // $gets = DeclarationStatistic::all();
+        $gets = Order::whereBetween('organizationDate',[$from_date, $to_date])
+                    ->select(DB::raw('count(id) as number'),'organizationDate')
+                    ->groupBy('organizationDate')
+                    ->orderBy('organizationDate','ASC')
+                    ->where('serviceId',$serviceId)
+                    ->get();
+                    // return json_encode($gets);
+
+        $chart_data = new Collection();
+        foreach($gets as $get){
+            $chart_data->push([
+                'organizationDate' => $get->organizationDate,
+                'number' => $get->number
+            ]);
+        }
+        return json_encode($chart_data);
     }
 
 }
